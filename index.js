f9703b8167b32257869bd2de07ad6eef60af2df5
AgradutFoundation <ac7605859@gmail.com>
2024-11-17 21:01:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function herf(){
+    window.location.href='join.html'
+}
+
+function href(){
+    window.location.href='join.html'
+}
+
+function het(){
+    window.location.href='https://forms.gle/ch1unVEgKUc7fNKG6'
+}
---
+++ j.js
@@ -0,0 +1,5 @@
+function herf(){
+    window.location.href='https://docs.google.com/forms/d/e/1FAIpQLScWZCaGd1TJLcDfheoAc5zHxkXBK8hzlTs0jI7RjpnfFzN88w/viewform?usp=sf_link'
+
+}
+
